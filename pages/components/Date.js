5ce3b0ec9d40e2441af1b75a2b0c340e5a14a0b5
pc-dev-05 <davioliveira.java@gmail.com>
2021-01-26 11:36:54 -0300

    Finishing all and pushing to vercel

--- pages/components/Date.js
+++ pages/components/Date.js
@@ -1,10 +1,9 @@
 import { format, parseISO } from "date-fns";
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString)
   return (
     <time dateTime={dateString}>
-      {format(date, 'LLLL d, yyyy')}
+      {dateString}
     </time>
   )
 }
